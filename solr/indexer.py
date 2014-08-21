6ce9dbf57f450c4416799eafe882becd693f95a2
Tom Mortimer <tom@flax.co.uk>
2014-08-21 16:02:33 +0100

    adds in bulk

--- solr/indexer.py
+++ solr/indexer.py
@@ -7,14 +7,22 @@ SOLR_URL = 'http://localhost:8983/solr/speedtest/update'
 
 def main():
     with gzip.open(sys.argv[1]) as f:
+        body = []
         for count, line in enumerate(f):
-            body = '[{0}]'.format(line)
-            resp = requests.post(SOLR_URL,
-                headers={'Content-Type': 'application/json'},
-                data=body)
-            assert resp.status_code == 200, resp.text
-            if count % 1000 == 0:
+            body.append(line)
+            if count % 100 == 0:
+                resp = requests.post(SOLR_URL,
+                    headers={'Content-Type': 'application/json'},
+                    data='[{0}]'.format(','.join(body)))
+                assert resp.status_code == 200, resp.text
                 print count
+                body = []
+
+    if body:
+        resp = requests.post(SOLR_URL,
+            headers={'Content-Type': 'application/json'},
+            data='[{0}]'.format(','.join(body)))
+            
     resp = requests.get(SOLR_URL + '?commit=true')
     assert resp.status_code == 200, resp.text
 
