ca5113f4fc6622ac5680e8507c94d220344164ee
Tom Mortimer <tom@flax.co.uk>
2014-08-26 16:53:43 +0100

    switched to bulk API

--- elasticsearch/indexer.py
+++ elasticsearch/indexer.py
@@ -3,19 +3,27 @@ import gzip
 import json
 import requests
 
-ES_URL = 'http://localhost:9200/speedtest/'
+ES_URL = 'http://localhost:9200/speedtest/_bulk'
 
 
 def main():
     with gzip.open(sys.argv[1]) as f:
-        for count, line in enumerate(f):
+        batch = []
+        count = 0
+        for line in f:
             doc = json.loads(line)
-            resp = requests.post("{0}{1}".format(ES_URL, doc['id']),
-                headers={'Content-Type': 'application/json'},
-                data=line)
-            assert resp.status_code == 201, (resp.status_code, resp.text)
-            if count % 1000 == 0:
+            batch.append(json.dumps({"index": 
+                {"_index": "speedtest", "_type": "document", "_id": doc["id"]}}))
+            batch.append(line.strip())
+
+            count += 1
+            if count % 100 == 0:
                 print count
+                resp = requests.post(ES_URL,
+                    headers={'Content-Type': 'application/octet-stream'},
+                    data='\n'.join(batch) + "\n")
+                assert resp.status_code == 200, (resp.status_code, resp.text)
+                batch = []
 
 if __name__ == '__main__':
     main()
